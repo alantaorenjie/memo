7d02c237a1b29b1d37a36c89fd08b2f69231d86e
trj <987687115@qq.com>
2018-11-30 11:42:32 +0800

    备忘录项目 spring boot 初始化

---
+++ src/main/java/com/trj/memo/MemoApplication.java
@@ -0,0 +1,13 @@
+package com.trj.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class MemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoApplication.class, args);
+    }
+}
