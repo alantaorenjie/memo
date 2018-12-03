f7d8842d6688b1cd32743190f69e74fc29c47102
trj <987687115@qq.com>
2018-12-03 16:40:04 +0800

    定时任务

--- src/main/java/com/trj/memo/App.java
+++ src/main/java/com/trj/memo/App.java
@@ -2,12 +2,13 @@ package com.trj.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class MemoApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemoApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
